aa6c7b3fa982ad4bbf92e1962922cd1ccbf0f3ac
Eviatar Zilberman <esztbk@gmail.com>
2024-01-14 18:21:44 +0200

    Add project files.

---
+++ ConfigurationsKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigApp
+{
+    public class ConfigurationsKeeper
+    {
+        public Dictionary<string, string> Data = new Dictionary<string, string>();
+
+        public ConfigurationsKeeper() 
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            foreach (string key in config.AppSettings.Settings)
+            {
+                this.Data.Add(key, config.AppSettings.Settings[key].Value); 
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Configuration;
+
+
+Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+/*// Update or add a setting
+//config.AppSettings.Settings["a"].Value = "AAA";
+// Save the changes to the configuration file
+config.Save(ConfigurationSaveMode.Modified);
+
+// Force a reload of the updated configuration file
+//System.Configuration.ConfigurationManager.RefreshSection("appSettings");*/
+var a = config.AppSettings.Settings["last"].Value;
+Console.WriteLine(a);
+
+
+
