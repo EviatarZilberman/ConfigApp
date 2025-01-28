9e6ef2e9ac585e71a35108fdc4280468509c676e
Eviatar Zilberman <esztbk@gmail.com>
2025-01-28 22:23:03 +0200

    Start of messuring requests mechanizm building.

--- ConfigurationsKeeper.cs
+++ ConfigurationsKeeper.cs
@@ -1,57 +1,106 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Configuration;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConfigApp
+//{
+//    public static class ConfigurationsKeeper
+//    {
+//        private static Dictionary<string, string> Data = new Dictionary<string, string>();
+//        private static Configuration? Config { get; set; } = null;
+//        //public ConfigurationsKeeper()
+//        //{
+//        //    Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+//        //    foreach (string key in config.AppSettings.Settings.AllKeys)
+//        //    {
+//        //        this.Data.Add(key, config.AppSettings.Settings[key].Value);
+//        //    }
+//        //}
+//        public static void Init()
+//        {
+//            if (Config == null)
+//            {
+//                lock (Data)
+//                {
+//                    Config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+//                    foreach (string key in Config.AppSettings.Settings.AllKeys)
+//                    {
+//                        Data.Add(key, Config.AppSettings.Settings[key].Value);
+//                    }
+//                }
+//            }
+//        }
+
+//        public static string GetValue(string key)
+//        {
+//            try
+//            {
+//                if (Config != null)
+//                {
+//                    lock (Data)
+//                    {
+//                        return Config.AppSettings.Settings[key].Value;
+//                    }
+//                }
+//                else
+//                {
+//                    return string.Empty;
+//                }
+//            }
+//            catch
+//            {
+//                return string.Empty;
+//            }
+//        }
+//    }
+
+
+//}
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConfigApp
 {
-    public static class ConfigurationsKeeper
+    public sealed class ConfigurationsKeeper
     {
-        private static Dictionary<string, string> Data = new Dictionary<string, string>();
-        private static Configuration? Config { get; set; } = null;
-        //public ConfigurationsKeeper()
-        //{
-        //    Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        // Singleton instance created lazily
+        private static readonly Lazy<ConfigurationsKeeper> _instance =
+            new Lazy<ConfigurationsKeeper>(() => new ConfigurationsKeeper());
 
-        //    foreach (string key in config.AppSettings.Settings.AllKeys)
-        //    {
-        //        this.Data.Add(key, config.AppSettings.Settings[key].Value);
-        //    }
-        //}
-        public static void Init()
+        // Thread-safe dictionary to store configuration
+        private readonly Dictionary<string, string> _data = new Dictionary<string, string>();
+        private readonly Configuration _config;
+
+        // Private constructor to prevent instantiation
+        private ConfigurationsKeeper()
         {
-            if (Config == null)
-            {
-                Config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            _config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
-                foreach (string key in Config.AppSettings.Settings.AllKeys)
-                {
-                    Data.Add(key, Config.AppSettings.Settings[key].Value);
-                }
+            // Populate the dictionary with configuration values
+            foreach (string key in _config.AppSettings.Settings.AllKeys)
+            {
+                _data[key] = _config.AppSettings.Settings[key].Value;
             }
         }
 
-        public static string GetValue(string key)
+        // Public static property to access the singleton instance
+        public static ConfigurationsKeeper Instance => _instance.Value;
+
+        // Method to get a configuration value
+        public string GetValue(string key)
         {
-            try
+            if (_data.TryGetValue(key, out var value))
             {
-                if (Config != null)
-                {
-                    return Config.AppSettings.Settings[key].Value;
-                }
-                else
-                {
-                    return string.Empty;
-                }
+                return value;
             }
-            catch 
-            {
-                return string.Empty;
-            }
+            return string.Empty;
         }
     }
-
-
-    }
+}
